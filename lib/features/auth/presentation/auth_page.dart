5c029c07ba9d53d62bf8c51ac1c3172008ef2461
Víctor Manuel Palmero Valdés <palmerodev@gmail.com>
2026-01-04 19:14:27 -0500

    ref: simplify auth page layout with sections

--- lib/features/auth/presentation/auth_page.dart
+++ lib/features/auth/presentation/auth_page.dart
@@ -1,16 +1,13 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
-import 'package:go_router/go_router.dart';
-import 'package:template/core/router/routes.dart';
 import 'package:template/core/theme/app_colors.dart';
-import 'package:template/core/utils/logger.dart';
 import 'package:template/core/utils/snackbar_helper.dart';
 import 'package:template/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:template/features/auth/presentation/bloc/auth_state.dart';
 import 'package:template/features/auth/presentation/sections/auth_form.dart';
 import 'package:template/features/auth/presentation/sections/auth_header.dart';
+import 'package:template/features/auth/presentation/sections/auth_toggle.dart';
 import 'package:template/i18n/strings.g.dart';
 
 class AuthPage extends StatefulWidget {
@@ -58,44 +55,14 @@ class _AuthPageState extends State<AuthPage> {
               strokeWidth: 3,
             ),
           ),
-          _ => Center(
+          _ => const Center(
             child: Column(
               children: [
-                const Header(),
-                const Gap(48),
-
-                const SingleChildScrollView(
-                  padding: EdgeInsets.symmetric(horizontal: 32.0),
-                  child: Column(
-                    mainAxisAlignment: .center,
-                    children: [
-                      AuthForm(),
-                      Gap(24),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                SafeArea(
-                  child: Text.rich(
-                    TextSpan(
-                      text: t.dont_have_an_account,
-                      children: [
-                        TextSpan(
-                          text: ' ${t.sign_up}',
-                          style: theme.textTheme.bodyMedium?.copyWith(
-                            letterSpacing: 0.5,
-                            fontWeight: .bold,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              logi('Debug');
-                              context.go(Routes.signUp.path);
-                            },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                Header(),
+                Gap(48),
+                AuthForm(),
+                Spacer(),
+                AuthToggle(),
               ],
             ),
           ),
